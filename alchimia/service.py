e32cd1312628af94e066d2e5e9409f0c6098eb04
Luca <luca@localhost.localdomain>
2014-06-06 19:18:45 +0200

    flask template added

--- alchimia/service.py
+++ alchimia/service.py
@@ -1,6 +1,7 @@
 ##spyne-flask webservice
 #prende in input gli url dei wsdl dei negozi e poi li quera uno per volta per ottenere i risultati
 from flask import Flask
+from flask import render_template
 from flask.ext.spyne import Spyne
 from spyne.protocol.soap import Soap11
 from spyne.model.primitive import Unicode, Integer
@@ -23,14 +24,14 @@ class RegisterService(spyne.Service):
     __service_url_path__ = '/soap/registrationservice'
     __in_protocol__ = Soap11(validator='lxml')
     __out_protocol__ = Soap11()
-    """os.path.dirname(os.path.realpath(__file__))
+    """
     this service generates a dictonary of registered shop which shall be queried for results
     """
     @spyne.srpc(Unicode, Unicode, _returns=Integer)
     def register_shop(shop_name, shop_url):
     	shops[shop_name] = shop_url
     	return 1
-
+###########################################
 #TODO
 """
 a function which connects to each shop in the dict and retrives albums throught their web service
@@ -38,19 +39,18 @@ a function which connects to each shop in the dict and retrives albums throught 
 def get_albums():
     srv_url = 'http://localhost:8000/?wsdl'
     c = Client(srv_url)
-    c.service.get_all_album()
+    return c.service.get_all_album()
 
 
 #####################
 #PRETTY HOMEPAGE PART YAY
 ######################
 @app.route('/cane')
+#@app.route('/cane/<query>')
 def cane():
-	shops['negozio2'] = "http://localhost:8002/?wsdl"
-	s = ""
-	for i in shops:
-		s += "<p>" + i +"\t"+ shops[i] + "</p>"
-	return s
+    rv = get_albums();
+    return render_template('service.html', rv=rv)
+
 
 @app.route('/album')
 def album():
