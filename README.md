3d75e9934cef661a553a415a671fa32ba4202cd0
Francesco Mastellone <francesco.mastellone@gmail.com>
2014-06-09 05:08:44 +0200

    Pulizia e design in preparazione per l'aggiunta delle query

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 progettosaed
 ============
 
-progetto web service soap
+A university project done for the Open & Distributed Systems course.
+
+We have to write a unified record store service, using SOAP.
+
+I don't see this being of much use for anyone but yay for open source.
--- service.py
+++ service.py
@@ -1,7 +1,11 @@
+"""Modulo che implementa il servizio unificato.
+
+Carica una lista dei negozi aderenti all'iniziativa,
+cerca di connettervisi,
+si mette a rispondere alle query.
+
 """
-spyne-flask webservice
-prende in input gli url dei wsdl dei negozi e poi li quera uno per volta per ottenere i risultati
-"""
+
 import logging
 h = logging.StreamHandler()
 rl = logging.getLogger()
@@ -15,38 +19,19 @@ from flask import render_template
 
 from suds.client import Client
 
-
-
-shops = json.load('shops.json')
+shop_urls = json.load('shops.json')
+shops = [Client(url) for url in shop_urls) #TODO: gestire riconnessioni
 
 app = Flask(__name__)
-spyne = Spyne(app)
 
-def get_all_albums():
-    for url in shops:
-        c = Client(url)
-        return c.service.get_all_album()
+def get_all_records():
+    for c in shops:
+        return c.service.get_all_records()
 
-def get_album(name):
-    raise NotImplementedError #TODO
-    
-
-@app.route('/cane')
-#@app.route('/cane/<query>')
+@app.route('/cane') #@app.route('/cane/?<query>')
 def cane():
-    rv = get_albums();
+    rv = get_all_records();
     return render_template('service.html', rv=rv)
-
-
-@app.route('/album')
-def album():
-    s = ""
-    srv_url = 'http://localhost:8000/?wsdl'
-    c = Client(srv_url)
-    x = c.service.get_all_album()
-    for i in x[0]:
-        s += "<p>" + i.title + "</p>"
-    return s
 
 if __name__ == '__main__':
     # db.create_all()
@@ -58,11 +43,5 @@ if __name__ == '__main__':
     # db.session.add(admin)
     # db.session.add(guest)
     # db.session.commit()
-    #debug andra tolto
-    app.debug = True
-    #il tutto viene avviato qui: http://localhost:porta/
-    app.run(port = 5000)
-
-if __name__ == '__main__':
     app.debug = True
     app.run(port = 5000)
