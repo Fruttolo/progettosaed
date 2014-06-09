8f35d7af68d739e9b0ef63884285ade773e2d5db
Francesco Mastellone <francesco.mastellone@gmail.com>
2014-06-09 03:30:02 +0200

    db relativo non assoluto

--- alchimia/servitore.py
+++ alchimia/servitore.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/luca/alchimia/prova.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///prova.db'
 db = SQLAlchemy(app)
 
 
